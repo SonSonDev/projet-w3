bfa92c1d239ed932536de902ed061d51d41d7755
Vincent Pham <tsvincent.pham@gmail.com>
2020-01-28 10:26:44 +0100

    first commit

---
+++ api/src/generated/prisma-client/index.js
@@ -0,0 +1,45 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prisma_lib_1 = require("prisma-client-lib");
+var typeDefs = require("./prisma-schema").typeDefs;
+
+var models = [
+  {
+    name: "Place",
+    embedded: false
+  },
+  {
+    name: "Address",
+    embedded: true
+  },
+  {
+    name: "Hour",
+    embedded: false
+  },
+  {
+    name: "Type",
+    embedded: false
+  },
+  {
+    name: "Day",
+    embedded: false
+  },
+  {
+    name: "Category",
+    embedded: false
+  },
+  {
+    name: "Vegan",
+    embedded: false
+  },
+  {
+    name: "Bio",
+    embedded: false
+  }
+];
+exports.Prisma = prisma_lib_1.makePrismaClientClass({
+  typeDefs,
+  models,
+  endpoint: `http://localhost:4466`
+});
+exports.prisma = new exports.Prisma();
---
+++ api/src/resolvers/Query.js
@@ -0,0 +1,8 @@
+async function place(parent, args, context) {
+  return await context.prisma.places()
+
+}
+
+module.exports = {
+  place,
+}
