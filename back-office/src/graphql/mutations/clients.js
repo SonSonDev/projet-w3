0781f0c87aade816052c5320241c59801966e311
Vincent Pham <thanh-son-vincent.pham@hetic.net>
2020-01-30 15:32:57 +0100

    create form style

--- back-office/src/graphql/mutations/clients.js
+++ back-office/src/graphql/mutations/clients.js
@@ -3,15 +3,13 @@ import gql from 'graphql-tag';
 export const CREATE_USER = gql`
 mutation CreateUser(
   $name: String!
-  $password: String!
   $email: String!
   $role: Role!
 ) {
-  createUser(name: $name, password: $password, email: $email, role: $role) {
+  createUser(name: $name, email: $email, role: $role) {
     id
     name
     email
-    password
     role
   }
 }
