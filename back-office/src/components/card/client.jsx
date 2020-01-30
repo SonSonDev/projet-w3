ceb5baabe32cc274fc78d895a5177a59316b39d8
Mahel Zeroual <mahel@bridge.audio>
2020-01-30 11:29:45 +0100

    é rajouté des grid

--- back-office/src/components/card/client.jsx
+++ back-office/src/components/card/client.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { DELETE_USER } from "../../graphql/mutations/clients";
 import { useMutation } from "@apollo/react-hooks";
-import { Card as Cards, Button, Header } from "tabler-react";
 import { Link } from "react-router-dom";
 
 const Card = ({ name, email, id }) => {
@@ -14,37 +13,34 @@ const Card = ({ name, email, id }) => {
   });
 
   return (
-    <Cards>
-      <Cards.Header>
-        <Cards.Title>NAME : {name}</Cards.Title>
-        <Cards.Options>
-          <Button color="red" size="sm" onClick={() => deleteUser()}>
+    <tr>
+      <td>{name}</td>
+      <td>placeholder type</td>
+      <td>placeholder adresse</td>
+      <td>ph count user</td>
+      <td>ph representative</td>
+      <td>
+        <div class="buttons">
+          <a class="button" onClick={() => deleteUser()}>
             Supprimer
-          </Button>
-          <Button
-            RootComponent={Link}
-            to={`/client/${id}/update`}
-            color="blue"
-            size="sm"
-          >
+          </a>
+        </div>
+      </td>
+      <td>
+        <div class="buttons">
+          <a to={`/client/${id}/update`} class="button">
             Update
-          </Button>
-          <Button
-            RootComponent={Link}
-            to={`/client/${id}`}
-            color="green"
-            size="sm"
-          >
+          </a>
+        </div>
+      </td>
+      <td>
+        <div class="buttons">
+          <a to={`/client/${id}`} class="button">
             Info
-          </Button>
-        </Cards.Options>
-      </Cards.Header>
-      <Cards.Body>
-        <Header.H3>Email</Header.H3>
-        {email}
-      </Cards.Body>
-      {/* <Cards.Footer>{role}</Cards.Footer> */}
-    </Cards>
+          </a>
+        </div>
+      </td>
+    </tr>
   );
 };
 
