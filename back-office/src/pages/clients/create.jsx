bb08b29f40f061ed80bc0f910c7b4c0c4397341c
Vincent Pham <thanh-son-vincent.pham@hetic.net>
2020-01-31 10:50:09 +0100

    remove useless password field + add link google maps on address

--- back-office/src/pages/clients/create.jsx
+++ back-office/src/pages/clients/create.jsx
@@ -21,10 +21,9 @@ const ClientCreate = () => {
   return (
     <section className="create">
       <Formik
-        initialValues={{ name: "", password: "", email: "", role: "ADMIN" }}
+        initialValues={{ name: "", email: "", role: "ADMIN" }}
         validationSchema={Yup.object({
           name: Yup.string(),
-          password: Yup.string(),
           email: Yup.string()
         })}
         onSubmit={(values, { setSubmitting }) => {
@@ -33,7 +32,6 @@ const ClientCreate = () => {
             createUser({
               variables: {
                 name: values.name,
-                password: values.password,
                 email: values.email,
                 role: values.role
               }
@@ -48,13 +46,6 @@ const ClientCreate = () => {
             <label htmlFor="name" className="label">Nom</label>
             <div className="control">
               <Field id="name" className="input" name="name" type="text" placeholder="Name" />
-            </div>
-          </div>
-
-          <div className="field">
-            <label htmlFor="password" className="label">Mot de passe</label>
-            <div className="control">
-              <Field id="password" className="input" name="password" type="password" placeholder="Mot de passe" />
             </div>
           </div>
 
