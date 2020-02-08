0c49029a998b4cc34c179d32bcd3e318ea97a81c
Vincent Pham <tsvincent.pham@gmail.com>
2020-02-08 10:23:11 +0100

    add ansible config

--- terraform/main.tf
+++ terraform/main.tf
@@ -44,6 +44,13 @@ resource "aws_security_group" "madu-security-group" {
   }
 
   ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     from_port   = 27017
     to_port     = 27017
     protocol    = "tcp"
