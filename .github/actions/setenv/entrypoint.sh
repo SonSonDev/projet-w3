fc306e942d924c84b19c81de58870f5a88114d6a
Théodore <theodoreyip19@gmail.com>
2020-06-24 22:04:06 +0200

    fix(github/workflows)

--- .github/actions/setenv/entrypoint.sh
+++ .github/actions/setenv/entrypoint.sh
@@ -4,8 +4,8 @@ echo $VAULT_PASS > ~/vault_pass
 ansible-vault decrypt .env.encrypted --output .env --vault-password-file=~/vault_pass
 
 set -a && . ./.env && set +a
-for str in $(printenv)
-do
-  echo "::set-env name=${str%%=*}::${str#*=}"
-  echo "::add-mask::${str%%=*}"
-done
+awk 'END { for (name in ENVIRON) {
+  print "::set-env name="name"::"ENVIRON[name];
+  print "::add-mask::"ENVIRON[name];
+  }
+}' < /dev/null
