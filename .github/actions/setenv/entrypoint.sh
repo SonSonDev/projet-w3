3715d6be9c14a2b072834fd6166bdc1ae9b66297
Théodore <theodoreyip19@gmail.com>
2020-06-24 21:08:10 +0200

    feat(github/workflows): use setenv action

--- .github/actions/setenv/entrypoint.sh
+++ .github/actions/setenv/entrypoint.sh
@@ -1,26 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 echo $VAULT_PASS > ~/vault_pass
 ansible-vault decrypt .env.encrypted --output .env --vault-password-file=~/vault_pass
 
-for var in $(cat .env) 
-do 
-  echo "::set-env name=$(echo $var | sed 's/=/::/')"
+set -a && . ./.env && set +a
+for str in $(printenv)
+do
+  echo "::set-env name=${str%%=*}::${str#*=}"
+  echo "::add-mask::${str%%=*}"
 done
-
-# set -a && . ./.env && set +a
-
-
-# echo $SSH_PRIVATE_KEY > ~/id_rsa
-# chmod 600 ~/id_rsa
-
-# cd ./automation/ansible
-# ansible-galaxy install -r requirements.yml
-# chmod 777 inventory/ec2.py
-
-# ansible-playbook get_elb_dns.yml -i inventory/hosts
-# echo "::set-env name=REACT_APP_API_URL::$(cat elb_api_dns.txt)"
-
-
-# echo $INPUT_CMD
-# eval $INPUT_CMD
